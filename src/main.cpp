a049214ed2140109fa19c08ce20c53837e395f17
Jonas Fehsenfeld <jonas.fehsenfeld@gmx.de>
2023-01-24 19:09:15 +0100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// WeatherStationServer.cpp : Defines the entry point for the application.
+//
+
+#include "WeatherStationServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
